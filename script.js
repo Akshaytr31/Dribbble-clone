1d26292b3c082b729f2be437aa8eaa0edec73a03
Akshaytr31 <akshaytr31@gmail.com>
2025-01-03 14:17:04 +0530

    3 commit

--- script.js
+++ script.js
@@ -215,75 +215,74 @@ document.addEventListener('click',()=>{
 
 
 
-    const filterBtn=document.getElementById('filterBtn')
-    const filterDrop=document.getElementById('filterDrop')
+const filterBtn=document.getElementById('filterBtn')
+const filterDrop=document.getElementById('filterDrop')
 
 
-    filterBtn.addEventListener('click',()=>{
-        if (filterDrop.classList.contains('visibleDrop')){
-            filterDrop.style.opacity="0"
-
-
-
-            setTimeout(()=>{
-                filterDrop.style.display="none"
-                filterDrop.classList.remove('visibleDrop')
-            },300)
-        }else{
-            filterDrop.style.display='block'
-
-
-            setTimeout(()=>{
-                filterDrop.style.opacity='1'
-                filterDrop.classList.add('visibleDrop')
-            },10)
-        }
-
-    })
+filterBtn.addEventListener('click',()=>{
+    if (filterDrop.classList.contains('visibleDrop')){
+        filterDrop.style.opacity="0"
 
 
 
+        setTimeout(()=>{
+            filterDrop.style.display="none"
+            filterDrop.classList.remove('visibleDrop')
+        },300)
+    }else{
+        filterDrop.style.display='block'
 
 
-
-    const scrollContainer = document.querySelector(".container_block2-scroll");
-    const backButton = document.querySelector(".scroll_span-back");
-    const forwardButton = document.querySelector(".scroll_span-forword");
-    const ulElement = document.querySelector(".container_block2-ul");
-    const scrollAmount = 300;
-
-    checkButtonsVisibility();
-
-    forwardButton.addEventListener("click", function (e) {
-        e.preventDefault();
-        ulElement.scrollBy({ left: scrollAmount, behavior: "smooth" });
-        setTimeout(checkButtonsVisibility, 300); 
-    });
-
-    backButton.addEventListener("click", function (e) {
-        e.preventDefault();
-        ulElement.scrollBy({ left: -scrollAmount, behavior: "smooth" });
-        setTimeout(checkButtonsVisibility, 300); 
-    });
-
-    function checkButtonsVisibility() {
-        const scrollLeft = ulElement.scrollLeft;
-        const scrollWidth = ulElement.scrollWidth;
-        const clientWidth = ulElement.clientWidth;
-
-        if (scrollLeft <= 0) {
-            backButton.style.display = "none";
-        } else {
-            backButton.style.display = "block";
-        }
-
-        if (scrollLeft + clientWidth >= scrollWidth) {
-            forwardButton.style.display = "none";
-        } else {
-            forwardButton.style.display = "block";
-        }
+        setTimeout(()=>{
+            filterDrop.style.opacity='1'
+            filterDrop.classList.add('visibleDrop')
+        },10)
     }
 
+})
+
+
+
+
+
+
+const scrollContainer = document.querySelector(".container_block2-scroll");
+const backButton = document.querySelector(".scroll_span-back");
+const forwardButton = document.querySelector(".scroll_span-forword");
+const ulElement = document.querySelector(".container_block2-ul");
+const scrollAmount = 300;
+
+checkButtonsVisibility();
+
+forwardButton.addEventListener("click", function (e) {
+    e.preventDefault();
+    ulElement.scrollBy({ left: scrollAmount, behavior: "smooth" });
+    setTimeout(checkButtonsVisibility, 300); 
+});
+
+backButton.addEventListener("click", function (e) {
+    e.preventDefault();
+    ulElement.scrollBy({ left: -scrollAmount, behavior: "smooth" });
+    setTimeout(checkButtonsVisibility, 300); 
+});
+
+function checkButtonsVisibility() {
+    const scrollLeft = ulElement.scrollLeft;
+    const scrollWidth = ulElement.scrollWidth;
+    const clientWidth = ulElement.clientWidth;
+
+    if (scrollLeft <= 0) {
+        backButton.style.display = "none";
+    } else {
+        backButton.style.display = "block";
+    }
+
+    if (scrollLeft + clientWidth >= scrollWidth) {
+        forwardButton.style.display = "none";
+    } else {
+        forwardButton.style.display = "block";
+    }
+}
 
 
 
@@ -296,45 +295,46 @@ document.addEventListener('click',()=>{
 
 
 
-    const dropdowns = document.querySelectorAll('.custom-dropdown');
 
-    dropdowns.forEach(dropdown => {
-      const selected = dropdown.querySelector('.drop-flex');
-      const optionsContainer = dropdown.querySelector('.dropdown-options');
-      const options = dropdown.querySelectorAll('.dropdown-options li');
-      const hiddenInput = dropdown.querySelector('input[type="hidden"]');
+const dropdowns = document.querySelectorAll('.custom-dropdown');
+
+dropdowns.forEach(dropdown => {
+  const selected = dropdown.querySelector('.drop-flex');
+  const optionsContainer = dropdown.querySelector('.dropdown-options');
+  const options = dropdown.querySelectorAll('.dropdown-options li');
+  const hiddenInput = dropdown.querySelector('input[type="hidden"]');
+
   
-      
-      selected.addEventListener('click', () => {
-        dropdowns.forEach(d => {
-          if (d !== dropdown) {
-            d.classList.remove('open');
-          }
-        });
-        dropdown.classList.toggle('open');
-      });
-  
-      options.forEach(option => {
-        option.addEventListener('click', () => {
-          dropdown.querySelector('.dropdown-selected').textContent = option.textContent;
-          hiddenInput.value = option.dataset.value;
-  
-          options.forEach(opt => opt.classList.remove('active'));
-          option.classList.add('active');
-  
-          dropdown.classList.remove('open');
-        });
-      });
+  selected.addEventListener('click', () => {
+    dropdowns.forEach(d => {
+      if (d !== dropdown) {
+        d.classList.remove('open');
+      }
     });
-  
-    document.addEventListener('click', e => {
-      dropdowns.forEach(dropdown => {
-        if (!dropdown.contains(e.target)) {
-          dropdown.classList.remove('open');
-        }
-      });
+    dropdown.classList.toggle('open');
+  });
+
+  options.forEach(option => {
+    option.addEventListener('click', () => {
+      dropdown.querySelector('.dropdown-selected').textContent = option.textContent;
+      hiddenInput.value = option.dataset.value;
+
+      options.forEach(opt => opt.classList.remove('active'));
+      option.classList.add('active');
+
+      dropdown.classList.remove('open');
     });
-      
+  });
+});
+
+document.addEventListener('click', e => {
+  dropdowns.forEach(dropdown => {
+    if (!dropdown.contains(e.target)) {
+      dropdown.classList.remove('open');
+    }
+  });
+});
+  
       
 
 
@@ -393,67 +393,67 @@ document.addEventListener('click',()=>{
 
 
 fetch('properties.json') 
-  .then(response => response.json())
-  .then(data => {
-   
-    const itemContainer = document.getElementById('item-list');
-
+.then(response => response.json())
+.then(data => {
   
-    data.items.forEach(item => {
-      const itemElement = document.createElement('li');
-      itemElement.innerHTML = `
-        <div class="block3-image2">
-          <img src="${item.image}" alt="">
-          <div class="hover">
-              <span>${item.hover}</span>
-              <div class=inner-items>
-                 <div class="save"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none" role="img" class="icon ">
-                         <path d="M3.33337 5.2C3.33337 4.0799 3.33337 3.51984 3.55136 3.09202C3.74311 2.71569 4.04907 2.40973 4.42539 2.21799C4.85322 2 5.41327 2 6.53337 2H9.46671C10.5868 2 11.1469 2 11.5747 2.21799C11.951 2.40973 12.257 2.71569 12.4487 3.09202C12.6667 3.51984 12.6667 4.0799 12.6667 5.2V14L8.00004 11.3333L3.33337 14V5.2Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                       </svg>
-                  </div>
-                  <div class="love">
-                       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none" role="img" class="icon ">
-                         <path d="M10.7408 2C13.0889 2 14.6667 4.235 14.6667 6.32C14.6667 10.5425 8.11856 14 8.00004 14C7.88152 14 1.33337 10.5425 1.33337 6.32C1.33337 4.235 2.91115 2 5.2593 2C6.60745 2 7.48893 2.6825 8.00004 3.2825C8.51115 2.6825 9.39263 2 10.7408 2Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                       </svg>
-                  </div>
+  const itemContainer = document.getElementById('item-list');
+
+
+  data.items.forEach(item => {
+    const itemElement = document.createElement('li');
+    itemElement.innerHTML = `
+      <div class="block3-image2">
+        <img src="${item.image}" alt="">
+        <div class="hover">
+            <span>${item.hover}</span>
+            <div class=inner-items>
+                <div class="save"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none" role="img" class="icon ">
+                        <path d="M3.33337 5.2C3.33337 4.0799 3.33337 3.51984 3.55136 3.09202C3.74311 2.71569 4.04907 2.40973 4.42539 2.21799C4.85322 2 5.41327 2 6.53337 2H9.46671C10.5868 2 11.1469 2 11.5747 2.21799C11.951 2.40973 12.257 2.71569 12.4487 3.09202C12.6667 3.51984 12.6667 4.0799 12.6667 5.2V14L8.00004 11.3333L3.33337 14V5.2Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+                      </svg>
+                </div>
+                <div class="love">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none" role="img" class="icon ">
+                        <path d="M10.7408 2C13.0889 2 14.6667 4.235 14.6667 6.32C14.6667 10.5425 8.11856 14 8.00004 14C7.88152 14 1.33337 10.5425 1.33337 6.32C1.33337 4.235 2.91115 2 5.2593 2C6.60745 2 7.48893 2.6825 8.00004 3.2825C8.51115 2.6825 9.39263 2 10.7408 2Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+                      </svg>
+                </div>
+            </div>
+        </div>
+      </div>
+      <div class="block3_prop">
+        <div class="block3_prop-main">
+          <div class="block3_prop-sub">
+            <img class="photo" alt="${item.author.name}" width="24" height="24" src="${item.author.avatar}">
+            <span>${item.author.name}</span>
+          </div>
+          <div class="block3_cat">
+            ${item.isPro ? '<a href="#" class="property-a2"><span class="pro">PRO</span></a>' : ''}
+            ${item.isTeam ? '<a href="#" class="property-a2"><span class="pro">TEAM</span></a>' : ''}
+          </div>
+          <div class="property-elm2">
+            <div class="property-elm2-like">
+              <div class="likes">
+                <a href="#">
+                  <img src="${item.likes.icon}">
+                  <span>${item.likes.count}</span>
+                </a>
               </div>
+            </div>
+            <div class="property-elm2-watch">
+              <div class="watch">
+                <a href="#">
+                  <img src="${item.watch.icon}">
+                  <span>${item.watch.count}</span>
+                </a>
+              </div>
+            </div>
           </div>
         </div>
-        <div class="block3_prop">
-          <div class="block3_prop-main">
-            <div class="block3_prop-sub">
-              <img class="photo" alt="${item.author.name}" width="24" height="24" src="${item.author.avatar}">
-              <span>${item.author.name}</span>
-            </div>
-            <div class="block3_cat">
-              ${item.isPro ? '<a href="#" class="property-a2"><span class="pro">PRO</span></a>' : ''}
-              ${item.isTeam ? '<a href="#" class="property-a2"><span class="pro">TEAM</span></a>' : ''}
-            </div>
-            <div class="property-elm2">
-              <div class="property-elm2-like">
-                <div class="likes">
-                  <a href="#">
-                    <img src="${item.likes.icon}">
-                    <span>${item.likes.count}</span>
-                  </a>
-                </div>
-              </div>
-              <div class="property-elm2-watch">
-                <div class="watch">
-                  <a href="#">
-                    <img src="${item.watch.icon}">
-                    <span>${item.watch.count}</span>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      `;
-      itemContainer.appendChild(itemElement);
-    });
-  })
-  .catch(error => console.error('Error loading JSON:', error));
+      </div>
+    `;
+    itemContainer.appendChild(itemElement);
+  });
+})
+.catch(error => console.error('Error loading JSON:', error));
 
 
 
@@ -654,3 +654,59 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
+
+
+
+
+
+const showAlert=document.getElementById('showAlert')
+const closeAlert=document.getElementById('closeAlert')
+const alertBox=document.getElementById('alertBox')
+const footer=document.querySelector('.footer')
+const nav=document.querySelector('.nav')
+const wrap=document.querySelector('.wrap')
+
+
+showAlert.addEventListener('click',()=>{
+  alertBox.style.display='block'
+  setTimeout(()=>{
+    alertBox.style.opacity='1'
+    alertBox.style.transform = 'scale(1)'
+  },10)
+  footer.classList.add('overlay')
+  nav.classList.add('overlay')
+  wrap.classList.add('overlay')
+})
+closeAlert.addEventListener('click',()=>{
+  alertBox.style.display='none'
+  alertBox.style.opacity='0'
+  alertBox.style.transform = 'scale(0.8)';
+  footer.classList.remove('overlay')
+  nav.classList.remove('overlay')
+  wrap.classList.remove('overlay')
+})
+
+
+
+
+
+
+document.getElementById('scrollTopButton').addEventListener('click',function(event){
+  event.preventDefault()
+  window.scrollTo({
+    top:0,
+    behavior:'smooth'
+  })
+})
+
+
+
+
+
+// to preventDefault behavior of all anchor tags
+
+document.addEventListener('click',function(event){
+  if(event.target.tagName==='A' || event.target.closest('a')){
+    event.preventDefault()
+  }
+})
